b796841c447b096ec4f68a7dad963db36ae6afc0
Cristyan Henrich <cristyanhenrich16@gmail.com>
2024-03-29 16:25:12 -0300

    Atualizações de conexão WebSocket e HTTP

--- server.js
+++ server.js
@@ -8,17 +8,21 @@ const server = createServer(app);
 const io = new Server(server);
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    const porta = socket.handshake.query.porta;
+    console.log('Conexão WebSocket estabelecida na porta:', porta);
 });
 
 app.get('/', (req, res) => {
+    //capturar a porta do cliente que está acessando a rota
+    const porta = req.headers.host.split(':')[1];
+    console.log('Conexão HTTP estabelecida na porta:', porta);
     res.send('Hello World');
 });
 
-app.listen(4002, () => {
-  console.log('server running at http://localhost:4002');
+server.listen(4001, () => {
+    console.log('server running at http://localhost:4001');
 });
 
-server.listen(4001, () => {
-  console.log('server running at http://localhost:4001');
+app.listen(4002, () => {
+    console.log('server running at http://localhost:4002');
 });
